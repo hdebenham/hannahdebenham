5d11569d6e8bb275f90937d3077151d3a56869fc
Hannah Debenham <hannahdebs213@gmail.com>
2020-01-07 18:22:51 -0700

    Working on work modal displays

---
+++ code/javascript/main.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+   
+    var sreWork = document.getElementById("sre-modal");
+
+    var sreBtn = document.getElementById("sre-btn");
+    
+    var close = document.getElementsByClassName("close")[0];
+
+    sreBtn.onclick = function() {
+        sreWork.style.display = "block";
+    }
+    
+    close.onclick = function() {
+        sreWork.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+        if(event.target == sreWork) {
+            sreWork.style.display = "none";
+        }
+    }
+}
