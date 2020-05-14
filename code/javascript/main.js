f7d1a4e8cdfd9d56cfb024dc1a3c31c7072dcd61
Hannah Debenham <hannahdebs213@gmail.com>
2020-05-14 14:50:55 -0400

    Updating Portfolio with content

--- code/javascript/main.js
+++ code/javascript/main.js
@@ -1,23 +1,55 @@
-window.onload = function() {
-   
-    var sreWork = document.getElementById("sre-modal");
-
+window.onload = function () {
+    
+    // BUTTON VARIABLES
     var sreBtn = document.getElementById("sre-btn");
+    var amsBtn = document.getElementById("ams-btn");
+    var anfBtn = document.getElementById("anf-btn");
+    var hstrBtn = document.getElementById("hstr-btn");
+    var maBtn = document.getElementById("ma-btn");
+    var ccdpBtn = document.getElementById("ccdp-btn");
     
-    var close = document.getElementsByClassName("close")[0];
-
-    sreBtn.onclick = function() {
-        sreWork.style.display = "block";
-    }
+    var BTNS = [sreBtn, amsBtn, anfBtn, hstrBtn, maBtn, ccdpBtn];
     
-    close.onclick = function() {
-        sreWork.style.display = "none";
-    }
+    var btnsLength = BTNS.length;
     
-    window.onclick = function(event) {
-        if(event.target == sreWork) {
-            sreWork.style.display = "none";
-        }
+    // WORK MODAL VARIABLES
+    var sreWork = document.getElementById("sre-modal");
+    var amsWork = document.getElementById("ams-modal");
+    var anfWork = document.getElementById("anf-modal");
+    var hstrWork = document.getElementById("hstr-modal");
+    var maWork = document.getElementById("ma-modal");
+    var ccdpWork = document.getElementById("ccdp-modal");
+    
+    var WORKS = [sreWork, amsWork, anfWork, hstrWork, maWork, ccdpWork];
+    
+    
+    
+    
+    for (var i = 0; i < btnsLength; i++) {
+            var btn = BTNS[i];
+            var close = document.getElementsByClassName("close")[i];
+        
+            (function(index) {
+                btn.onclick = function() {
+                    WORKS[index].style.display = "block";
+                    console.log(index);
+                    
+                }
+                
+                close.onclick = function() {
+                    WORKS[index].style.display = "none";
+                }
+                
+                WORKS[index].onclick = function() {
+                    WORKS[index].style.display = "none";
+                }
+                
+            })(i);
+    
         
     }
+    
+    
 }
+
+
